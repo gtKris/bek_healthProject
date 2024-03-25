63066d2450184f877458bbbb3bc6035b1bff415e
gtKris <titobaltodanoperez@gmail.com>
2024-03-25 11:23:51 -0600

    Add customer DAO , DTO and CRUD  VIEWS

--- bek_healthProject/App_Start/RouteConfig.cs
+++ bek_healthProject/App_Start/RouteConfig.cs
@@ -13,7 +13,7 @@ namespace bek_healthProject
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            routes.MapRoute( 
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Customer", action = "Index", id = UrlParameter.Optional }
